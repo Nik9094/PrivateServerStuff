3f7060140a0ac0747da3571a82f44c34095f55fa
Nik9094 <nicholasdilazzaro@gmail.com>
2023-04-05 19:24:11 +0200

    Finalized initial permissions command

--- src/main/java/me/nik/serverstuff/SelfPermissions.java
+++ src/main/java/me/nik/serverstuff/SelfPermissions.java
@@ -1,6 +1,6 @@
 package me.nik.serverstuff;
 
-//import org.bukkit.Bukkit;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +11,11 @@ public class SelfPermissions implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage("/lp user " + player.getName() + " group parent add player");
-            //Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "/lp user " + player.getName() + " group parent add player");
+            if (player.hasPermission("getreal.use")) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " parent add player");
+                player.sendMessage("§AYou now have all necessary permissions to play!");
+            } else
+                player.sendMessage("§CMissing permission getreal.use!");
         }
         return true;
     }
